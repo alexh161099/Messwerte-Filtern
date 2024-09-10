30e8d3fffe9da983a097b876e6db62ded64b8f1f
alexh161099 <aholzenkamp@web.de>
2024-09-10 11:04:38 +0200

    Create Messwerte-Filtern.py
    Ein Programm zum erkennen von Mittelwert abzüglich des höchsten und niedrigsten Wertes. Programm auf Funktion geprüft.

---
+++ Messwerte-Filtern.py
@@ -0,0 +1,37 @@
+""" Name: Messwerte filtern
+### Version: 1.0.0
+### Autor: Alexander Holzenkamp
+### Datum: 10.09.2024
+### Änderungen:
+### Messwerte mit append hinzugefügt,
+### Liste sortiert und Werte gelöscht,
+### Mittelwert gebildet und gerundet,
+### Programm auf funktion geprüft
+"""
+
+#Liste mit Messwerten in Celcius
+messwerte = []
+messwerte.append (15.2)
+messwerte.append (13.5)
+messwerte.append (13.6)
+messwerte.append (14.7)
+messwerte.append (16.9)
+messwerte.append (14.8)
+messwerte.append (17.8)
+messwerte.append (14.9)
+messwerte.append (16.4)
+messwerte.append (15.0)
+
+#Liste sortieren und Messwerte löschen
+messwerte.sort()
+kleinster_wert = min(messwerte)
+groesster_wert = max(messwerte)
+
+messwerte.remove(kleinster_wert)
+messwerte.remove(groesster_wert)
+
+#Mittelwert bilden und zahlen runden
+mittelwert = sum(messwerte) /len(messwerte)
+mittelwert= round(mittelwert)
+print("Der Mittelwert ist", mittelwert ,"Grad +- 0,3")
+
